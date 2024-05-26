1a38c78b6731014d0818a2021205f3c32d91c282
Marcos Simoneli <marcossimoneli@hotmail.com>
2024-05-26 19:32:20 -0300

    update

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -1,7 +1,14 @@
-﻿public class Program
+﻿using Algorithms;
+
+#pragma warning disable S125 // Sections of code should not be commented out
+public class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hello World!");
+        //int[] array = { 1, 2, 3, 4, 5 };
+        //int number = 5;
+        //var found = SimpleSearch.SimpleSearchMethod(array, number);
+
+        //Console.WriteLine($"O numero esta no array? {found}!");
     }
 }
---
+++ Algorithms/SimpleSearch.cs
@@ -0,0 +1,16 @@
+namespace Algorithms
+{
+    public class SimpleSearch
+    {
+        public static bool SimpleSearchMethod(int[] array, int number)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == number)
+                    return true;
+            }
+
+            return false;
+        }
+    }
+}
